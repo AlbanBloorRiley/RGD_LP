9353f563d2fd6ac596d99612b5b3ebacccd795fb
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-01-23 15:58:31 +0000

    Added Mn6 example

--- LPpaperExamples.m
+++ LPpaperExamples.m
@@ -317,23 +317,66 @@ LPnewTime = toc/repeats;
 
 
 
-% ekLPnew = vecnorm(newIterations.Iterates(:,2:end)-newIterations.Iterates(:,1:end-1),2,1);
-
-
-
 %% Example 5 - Mn6 
 clear all
 rcm = 29979.2458;    % reciprocal cm to MHz
 meV = rcm*8.065;
 
-problem.ev = [ 0, 0.1414, 0.59070, 0.59070, 1.0841, 1.0841 , 1.0841, 1.4134, 1.4134, 2.316, 2.316, 2.316, 2.5218, 2.5218, 2.5218, 2.5218]';  
+problem.ev = [ 0, 0.1414, 0.59070, 0.59070, 1.0841, 1.0841 , 1.0841, 1.4134, 1.4134, 2.316, 2.316, 2.316, 2.5218, 2.5218, 2.5218, 2.5218]'.*meV;  
+Sys1.S = [2 2 5/2 5/2 5/2 5/2]; % MnIII has S = 2, MnII has S = 5/2
 
+clear A
+Sys1.J = [10*meV,0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+problem.A0 = ham_ee(Sys1,1:length(Sys1.S),'sparse');
+A{1} = stev(Sys1.S,[2,0,1],'sparse')+ stev(Sys1.S,[2,0,2],'sparse');
+% A{end+1} = stev(Sys1.S,[2,2,1],'sparse')+ stev(Sys1.S,[2,2,1],'sparse');
+Sys1.J = [0,1 0 1 0 0 1 0 1 0 0 0 0 0 0];
+A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse');
+Sys1.J = [0,0 1 0 1 1 0 1 0 0 0 0 0 0 0];
+A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse');
+% A{end} = A{end}-ham_ee(Sys1,1:length(Sys1.S),'sparse');
+
+% Sys1.J = [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+% A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse'); 
+Sys1.J = [0 0 0 0 0 0 0 0 0 1 0 0 0 0 1];
+A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse');
+
+problem.Solver = @mldivide;  problem.obj_fun = @INSEvaulate;
+f = spyplots(2,2,A{1},A{2},A{3},A{4});
+
+%%
+problem.x0 = [-21035 -1.9826e+05 1.9343e+05  48357 ]';
+% problem.x0 = [-21035 -1.9826e+05  48357 ]';
+problem.x0 = [2000000 -20000 -200000   50000 ]';
+% problem.x0 = [-10000 -100000 100000  10000 ]';
+problem.x0(end+1) = -eigs(FormA(problem.x0,A,problem.A0),1,"smallestreal");
+% problem.x0(end) = 1;
+A{5} = speye(32400,32400);problem.A = A; 
+%
+% xscaling = problem.x0; problem.A = scaleAx(problem.x0,A); problem.x0 = ones(length(problem.x0),1);
+repeats = 1;
+% % problem.x0 = ones(length(problem.x0),1)
+epsilon = 8; LPSteps = 100; doubled = false; NewtonSteps = 0;
+tic
+for i = 1:repeats
+    % [LPIterations,NewtonIterations] = Old_LP_Newton(problem,epsilon,LPSteps,NewtonSteps);
+    [LPIterations,NewtonIterations] = RGD_LP_Newton(problem,epsilon,LPSteps,NewtonSteps, doubled);
+end
+LPnewTime = toc/repeats;
+LPIterations
+%%
+[Q,D] = eigs(FormA(LPIterations.FinalPoint, problem.A,problem.A0),length(problem.ev),'smallestreal');
+ [Q0,D0] = eigs(FormA(problem.x0, problem.A,problem.A0),length(problem.ev),'smallestreal');
+D0 = diag(D0);% D0 =  (D0-D0(1))./meV;
+D = diag(D); %D = (D-D(1))./meV;
+MintOpt.Eigs = D;
+MintOpt.Vecs = Q;
+%%
 Sys.S = [2 2 5/2 5/2 5/2 5/2]; % MnIII has S = 2, MnII has S = 5/2
-
 J_S4_S4   = -5*meV;    % MnIII - MnIII.                       Rodolphe: Strong and AFM.    Keep fixed.
-J_S4_S5_1 = 0.41*meV;  % MnIII - MnII, MnIII JT involved.     Rodolphe: Weak, FM or AFM.   Free value
-J_S4_S5_2 = -0.4*meV; % MnIII - MnII, MnIII JT not involved. Rodolphe: Weak and AFM.      Free value
-J_S5_S5   = -0.1*meV; 
+J_S4_S5_1 = LPIterations.FinalPoint(2)./(-2);  % MnIII - MnII, MnIII JT involved.     Rodolphe: Weak, FM or AFM.   Free value
+J_S4_S5_2 = -LPIterations.FinalPoint(2)./(-2); % MnIII - MnII, MnIII JT not involved. Rodolphe: Weak and AFM.      Free value
+J_S5_S5   = LPIterations.FinalPoint(3)./(-2); 
 J_AB = J_S4_S4;
 J_A1 = J_S4_S5_1; J_A3 = J_S4_S5_1; J_B2 = J_S4_S5_1; J_B4 = J_S4_S5_1; %For these, the MnIII JT axis is involved. Can be FM or AFM
 J_A2 = J_S4_S5_2; J_A4 = J_S4_S5_2; J_B1 = J_S4_S5_2; J_B3 = J_S4_S5_2; %For these, the MnIII JT axis is NOT involved. Can only be AFM
@@ -341,14 +384,10 @@ J_12 = J_S5_S5;   J_34 = J_S5_S5;
 J_14 = -0.00.*meV;         J_23 = J_14; %Assumed zero. Only interacts via a pivalate bridge.
 J_13 = -0.00.*meV;         J_24 = J_13; 
 Sys.J = [J_AB J_A1 J_A2 J_A3 J_A4 J_B1 J_B2 J_B3 J_B4 J_12 J_13 J_14 J_23 J_24 J_34].*(-2); %-2JS.S formalism
-
-DIII = -0.029*meV; % MnIII anisotropy. Free Value 
+DIII = LPIterations.FinalPoint(1)./3; % MnIII anisotropy. Free Value 
 % DIII = -0.02*meV;
-
 EIII = 0*DIII;   % MnIII rhombicity. For INS, assume 0.
 B20III = 3*DIII; B22III = EIII; %Converting to Stevens Operator formalism
-
-
 DII = -0.00*meV; % MnII anisotropy. For INS, assume 0. 
 EII = DII*0;      % MnII rhombicity. For INS, assume 0.
 B20II = 3*DII; B22II = EII; %Converting to Stevens Operator formalism
@@ -358,52 +397,39 @@ Sys.B2 = [B22III 0 B20III 0 0;
           B22II 0 B20II 0 0;
           B22II 0 B20II 0 0;
           B22II 0 B20II 0 0];
+MintSys = Sys;
+
+FormFactor = {'Mn3', 'Mn3', 'Mn2', 'Mn2', 'Mn2', 'Mn2'};
+% Metal ion coordinates for INS. Only relative positions are important.
+Coords = [24.60550  13.06674   7.07523; % A
+    22.27025  11.49336   6.95626; % B
+    21.99544  13.57222   9.30268; % 1
+    24.62949  10.95050   9.41655; % 2
+    24.24486  10.55088   4.68547; % 3
+    22.84040  14.03029   4.66904; % 4
+    ];
+MintSys.FormFactor = FormFactor;
+MintSys.Coords = Coords;
+% Sim INS powder spectrum
+MintExp.SpectrumType = 'SE'; %INS intensity vs. energy
+Ei = 4; %Incident neutron energy in meV
+MintExp.lwfwhm = 0.02*Ei/2.355; %calculated line width full-width-at-half-max, 2 % of incident energy
+% MintExp.lwfwhm = 0.05;
+MintExp.Energy = linspace(-Ei*0.8,Ei*0.8,1000); %calculating the spectrum in the interval -0.8*Ei to 0.8*Ei
+MintExp.Q = 0.1:0.01:2.5; %Q-range which the simulation integrates over.
+MintExp.Temperature = [1.5 5 10 30];
+MintOpt.NumEigs = length(problem.ev); %100 eigenvalues gives a good INS sim
+
+b =[0 0.4470 0.7410];
+r=[0.8500 0.3250 0.0980];
+y=[0.9290 0.6940 0.1250];
+g=[0.4660 0.6740 0.1880];
+colours = [b;y;g;r];
+
+    [cross_sect,Eigs,Vecs,I_nm] = mint(MintSys,MintExp,MintOpt);
+    plotmint(cross_sect, MintExp, colours)
+
 %%
-clear A
-Sys1.S = Sys.S;
-Sys1.J = [1,0 0 0 0 0 0 0 0 0 0 0 0 0 0];
-problem.A0 = ham_ee(Sys1,1:length(Sys1.S),'sparse');
-A{1} = stev(Sys1.S,[2,0,1],'sparse')+ stev(Sys1.S,[2,0,2],'sparse');
-% A{end+1} = stev(Sys1.S,[2,2,1],'sparse')+ stev(Sys1.S,[2,2,1],'sparse');
-Sys1.J = [0,1 0 1 0 0 1 0 1 0 0 0 0 0 0];
-A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse');
-Sys1.J = [0,0 1 0 1 1 0 1 0 0 0 0 0 0 0];
-A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse');
-Sys1.J = [0 0 0 0 0 0 0 0 0 1 0 0 0 0 1];
-A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse');
-
-problem.Solver = @mldivide;  problem.obj_fun = @INSEvaulate;
-f = spyplots(1,3,A{1},A{2},A{3});
-
-%%
-A{5} = speye(32400,32400);problem.A = A; 
-problem.x0 = [-21035 -1.9826e+05 1.9343e+05  48357 1]';
-
-%
-repeats = 1;
-epsilon = 1e-5; LPSteps = 10; doubled = false; NewtonSteps = 0;
-tic
-for i = 1:repeats
-    % [LPIterations,NewtonIterations] = Old_LP_Newton(problem,epsilon,LPSteps,NewtonSteps);
-    [LPIterations,NewtonIterations] = RGD_LP_Newton(problem,epsilon,LPSteps,NewtonSteps, doubled);
-end
-LPnewTime = toc/repeats;
-NewtonIterations
-%%
-
-epsilon = 1e-10; LPSteps = 10; doubled = false; NewtonSteps = 0;
-tic
-for i = 1:repeats
-    % [LPIterations,NewtonIterations] = O ld_LP_Newton(problem,epsilon,LPSteps,NewtonSteps);
-    [LPIterations,NewtonIterations] = LP_Newton(problem,epsilon,LPSteps,NewtonSteps);
-end
-LPnewTime = toc/repeats;
-
-
-
-% ekLPnew = vecnorm(newIterations.Iterates(:,2:end)-newIterations.Iterates(:,1:end-1),2,1);
-
-
 
 %% Aux Funcs
 
@@ -512,8 +538,8 @@ end
 
 function [F,R,J,H] = INSEvaulate(x,constants)
 A = constants.A;
-Ad = x(1)*A{1};
-for i = 2:length(x)
+Ad = constants.A0;
+for i = 1:length(x)
     Ad = Ad + x(i)*A{i};
 end
 if length(A{1})>500 && length(constants.ev)<0.5*length(A{1})&&nargout<4
@@ -521,7 +547,8 @@ if length(A{1})>500 && length(constants.ev)<0.5*length(A{1})&&nargout<4
     D = diag(D);
 elseif nargout>4&&length(A{1})>5000
 else
-    [Q,D] = eigs((Ad),length(A{1}),'vector');
+    [Q,D] = eigs((Ad),length(A{1}));
+    D = diag(D);
     D = D(1:length(constants.ev));
     Q = Q(:,1:length(constants.ev));
 end
@@ -559,51 +586,6 @@ if nargout>3
 end
 end
 
-function [F,R,J,H] = INSEvaulate_(x,constants)
-A = constants.A;
-Ad = x(1)*A{1};
-for i = 2:length(x)
-    Ad = Ad + x(i)*A{i};
-end
-if length(A{1})>500 && length(constants.ev)<0.5*length(A{1})&&nargout<4
-    [Q,D] = eigs(Ad, length(constants.ev), 'smallestreal');
-    D = diag(D);
-else
-    [Q,D] = eig(full(Ad),'vector');
-    D = D(1:length(constants.ev));
-    Q = Q(:,1:length(constants.ev));
-end
-F = sqrt(sum((D-constants.ev).^2));
-if nargout>1
-    R = (D-constants.ev);
-end
-if nargout>2
-    l = length(A);
-    m = size(Q,2);
-    J = zeros(m,l);
-    for k = 1:l
-        J(:,k) =real(sum((Q.'*A{k}).*Q',2));
-    end
-end
-if nargout>3
-    m = length(constants.ev); l=length(A);
-    H = zeros(l,l,m);
-    QAQ = cell(1,l);
-    for i = 1:l
-        QAQ{i} = Q'*A{i}*Q;
-        QAQ{i} =QAQ{i}(1:m,1:m);
-    end
-    DD=D'-D;
-    DD(abs(DD)<1e-15) = Inf;
-    for j=1:l
-        for k = 1:l
-            H(k,j,:) = real(2*sum(QAQ{k}.*QAQ{j}./DD));
-        end
-    end
-end
-end
-
-
 function setMarkerNumber(f,n)
     for i = 1:length(f.Children)
         f.Children(i).MarkerIndices = 1:(floor(length(f.Children(i).MarkerIndices)/n)):f.Children(i).MarkerIndices(end);
@@ -611,13 +593,36 @@ function setMarkerNumber(f,n)
 end
 
 function f = spyplots(x,y,varargin)
-    l = length(varargin);
-    if l~= x*y
-        error("must be the same dimension")
-    end
-    f = figure(1);
+l = length(varargin);
+if l~= x*y
+    error("must be the same dimension")
+end
+f = figure(1);
 for i = 1:l
     subplot(x,y,i)
     spy(varargin{i})
 end
+end
+
+function A = scaleAx(x,A)
+for i = 1:length(x)
+    A{i} = A{i}*x(i);
+end
+end
+
+function plotmint(cross_sect, MintExp, colours)
+subplot(2,1,1)
+plot(MintExp.Energy,cross_sect*1e-4,'linewidth',1.2)
+legend('1.5 K', '5 K', '10 K', '30 K')
+xlim([0 1.5]);xlabel('E [meV]');ylabel('Signal');title('New Sim')
+ax = gca;;ax.ColorOrder = colours;
+%
+ subplot(2,1,2)
+load("Sys0_Sim.mat")
+plot(MintExp.Energy,cross_sect_Sys0*1e-4,'linewidth',1.2)
+legend('1.5 K', '5 K', '10 K', '30 K')
+xlim([0 1.5]);xlabel('E [meV]');ylabel('Signal');title('Old Sim')
+ax = gca;;ax.ColorOrder = colours;
+f.Units = 'centimeters';
+f.Position = [10 10 20 25];
 end
