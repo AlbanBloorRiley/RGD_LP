a42fdd748982c6aa49fd5a438b38dc8997e2cb49
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-01-29 16:33:26 +0000

    Update FormA.m

--- FormA.m
+++ FormA.m
@@ -1,19 +1,19 @@
-function Ad = FormA(d,A,A0)
+function Ad = FormA(d,A,varargin)
 %Forms Current estimate for the Desired Matrix
 %
 %Inputs
 %d - current estimate for the parameters
-%A and A0 - The basis matrices used
-
-Ad = A0+d(1)*A{1};
-if iscell(A)
-    for i = 2:length(d)
-        Ad = Ad + d(i)*A{i};
-    end
+%A and A0 - The basis matrices used (A0 is optional)
+if nargin < 3
+    Ad = d(1)*A{1};
+elseif nargin == 3
+    Ad = varargin{1}+d(1)*A{1};
 else
-    for i = 2:length(d)
-        Ad = Ad + d(i)*A(:,:,i);
-    end
+    error("incorect number of inputs")
+end
+
+for i = 2:length(d)
+    Ad = Ad + d(i)*A{i};
 end
 
 end
