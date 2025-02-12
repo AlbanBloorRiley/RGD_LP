8cf2f75efcf1eddcd566025130e310e1b1791722
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-02-12 16:25:13 +0000

    Create IEP.m

---
+++ IEP.m
@@ -0,0 +1,36 @@
+function [f,varargout] = IEP(x,constants)
+if length(constants.A{1})<500 
+    [QFull,DFull] = eig(full(FormA(x,constants.A,constants.A0)),'vector');    
+elseif length(constants.ev)<0.5*length(constants.A{1}) &&nargout<4
+    [Q,D] = eigs(FormA(x,constants.A,constants.A0),length(constants.ev),constants.ev);
+else
+    [QFull,DFull] = eig(full(FormA(x,constants.A,constants.A0)),'vector');
+    if length(constants.ev)<length(DFull)
+        C = (DFull'-constants.ev).^2;
+        pairs = matchpairs(C,100*max(max(C)));
+        D = DFull(pairs(:,2));
+        Q = QFull(:,pairs(:,2));
+        % constants.ev = constants.ev((pairs(:,1)));
+    else
+        D = DFull; Q = QFull;
+    end
+end
+if any(isnan(D))
+    f = nan;
+    for i = 1:nargout-1
+        varargout{i} = nan;
+    end
+    return
+end
+
+f = sqrt(sum(((D-constants.ev)).^2));
+if nargout>1
+    varargout{1} = (D-constants.ev);
+end
+if nargout>2
+    varargout{2} =  FormJ_Lambda(Q,constants.A);
+end
+if nargout>3
+    varargout{3} = FormH_Lambda(QFull,constants.A,DFull,constants.ev);
+end
+end
