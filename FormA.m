bdb335e4ab5bd6f35b32e59d11b70211bb6433c8
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-02-16 01:03:16 +0000

    Made calculation of A(x) more efficient

--- FormA.m
+++ FormA.m
@@ -1,19 +1,55 @@
-function Ad = FormA(d,A,varargin)
+function Ad = FormA(x,A,varargin)
 %Forms Current estimate for the Desired Matrix
 %
 %Inputs
 %d - current estimate for the parameters
 %A and A0 - The basis matrices used (A0 is optional)
-if nargin < 3
-    Ad = d(1)*A{1};
-elseif nargin == 3
-    Ad = varargin{1}+d(1)*A{1};
-else
+if nargin>3
     error("incorect number of inputs")
 end
+if iscell(A)
+I = [];
+J = [];
+V = [];
+n = 0;
+for k = 1:length(A)
+    [i,j,v] = find(A{k});
+    p = n + numel(i);
+    m = numel(I);
+    if p > m
+        m = max(p,2*m);
+        I(m) = 0;
+        J(m) = 0;
+        V(m) = 0;
+    end
+    idx = (n+1:p);
+    I(idx) = i;
+    J(idx) = j;
+    V(idx) = v*x(k);
+    n = p;
+end
+idx = (n+1:numel(I));
+I(idx) = [];
+J(idx) = [];
+V(idx) = [];
+[m,n] = size(A{1});
+Ad = sparse(I,J,V,m,n);
 
-for i = 2:length(d)
-    Ad = Ad + d(i)*A{i};
+if nargin == 3
+    Ad = varargin{1}+Ad;
+end
+elseif isstruct(A)
+
+lidx =1; v = zeros(sum(A.NumV),1);
+    for k = 1:length(x)
+        uidx = lidx+A.NumV(k)-1;
+        v(lidx:uidx) = A.v(lidx:uidx)*x(k);
+        lidx =uidx+1;
+    end
+Ad = sparse(A.i,A.j,v,A.m,A.n);
+
+else
+    error("A must be a cell array of basis matrices, or a structure used to construct a sparse matrix")
 end
 
 end
