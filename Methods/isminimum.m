ffbd4a92ed4b6451dcb1c3f45fdfb3c1110004c2
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-03-17 17:59:14 +0000

    Added relative and difference stoping criteria

--- Methods/isminimum.m
+++ Methods/isminimum.m
@@ -1,4 +1,4 @@
-function [test,flag] = isminimum(f,p,NIter,ConvergenceParams)
+function [test,flag] = isminimum(f,x,p,pprev,NIter,ConvergenceParams)
 flag = "";
 test = false;
 if norm(p)<ConvergenceParams.StepTolerance
@@ -10,5 +10,11 @@ elseif isnan(f)||isinf(f)
 elseif NIter>=ConvergenceParams.MaxIter
     flag ='Max Iterations reached';
     test = true  ;
+elseif isfield(ConvergenceParams,'RelativeStepTolerance')&& (norm(p)/norm(x))<ConvergenceParams.RelativeStepTolerance
+    flag = 'Relative Step Size below tolerance';
+    test=true;
+elseif isfield(ConvergenceParams,'StepDifferenceTolerance')&& abs(norm(p)-norm(pprev))<ConvergenceParams.StepDifferenceTolerance
+    flag = 'Step Size Difference below tolerance';
+    test=true;
 end
 end
