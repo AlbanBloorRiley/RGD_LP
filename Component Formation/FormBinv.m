220a1ab9e174545923efd7e8e908cfdd5d126cce
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-03-19 10:39:45 +0000

    Added cholesky factorisation

--- Component Formation/FormBinv.m
+++ Component Formation/FormBinv.m
@@ -1,13 +1,17 @@
-function [Binv,B]= FormBinv(A)
+function [Binv,B,CholR]= FormBinv(A)
 l = length(A);
 B = zeros(l);
 for i = 1:(l)
     B(i,i) = sum(sum(A{i}'.*A{i}));
     for j = i+1:(length(A))
-        B(i,j) = sum(sum(A{j}'.*A{i}));
+        % B(i,j) = sum(sum(A{j}'.*A{i}));
+        B(i,j) = A{j}(:)'*A{i}(:);
         B(j,i) = B(i,j);
     end
 end
 B = sparse(B);
 Binv = inv(B);
+if nargout>2
+    CholR = chol(B);
+end
 end
