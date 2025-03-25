20acef7d1e8fcce9de29b285327d00246d57d1b7
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-03-25 15:35:53 +0000

    Update OGLP.m

--- Methods/OGLP.m
+++ Methods/OGLP.m
@@ -1,22 +1,29 @@
 function [CurrentLoop] = OGLP(constants)
-obj_fun = constants.obj_fun; NIter = 0;   x = constants.x0;      
+obj_fun = constants.obj_fun; NIter = 0;   x = constants.x0; l = length(x);
+N = length(constants.A{1});
 CurrentLoop.Iterates = x;
-pprev = 0;
+% pprev = 0;
 Binv = FormBinv(constants.A);
+
+if isfield(constants, 'Verbose')
+    Verbose = constants.Verbose;
+else
+    Verbose = false;
+end
 
 % Calculate residual, Jacobian and Hessian of R
 [X.F,X.R,X.J] = obj_fun(x, constants); FuncCount = 1;
 [stop,CurrentLoop.ConvergenceFlag] = isminimum(X.F,x, inf,inf, NIter, constants);
-
-OutputLineLength = fprintf('k = %d; f(x) = %d; |gradf(x)| = %d\n', NIter,X.F,norm(X.J'*X.R));
-fprintf(repmat(' ',1,OutputLineLength))
-
+if Verbose
+    OutputLineLength = fprintf('k = %d; f(x) = %d; |gradf(x)| = %d\n', NIter,X.F,norm(X.J'*X.R));
+    fprintf(repmat(' ',1,OutputLineLength))
+end
 % Main Loop
 while stop == false
 
     [QFull,DFull] = eig(full(FormA(x,constants.A,constants.A0)),'vector');
-    
-    if length(constants.ev)<length(DFull)
+
+    if length(constants.ev)<N
         C = (DFull'-constants.ev).^2;
         pairs = matchpairs(C,100*max(max(C)));
         % D = DFull(pairs(:,2));
@@ -26,25 +33,22 @@ while stop == false
         Qmatch = QFull(:,pairs(:,2));
         Qcomp = QFull;
         Qcomp(:,pairs(:,2)) =[];
-        % constants.ev = constants.ev((pairs(:,1)));
-        Q = [Qmatch,Qcomp];
-        % if any(pairs(:,2)~=[1:length(pairs)]')
-        %     pairs
-        % end
+        
+         Z = [Qmatch,Qcomp]*diag([constants.ev;Dcomp])*[Qmatch,Qcomp]';
     else
-         Q = QFull;  %D = DFull;
-         Dcomp = [];
+        Z = QFull*diag(constants.ev)*QFull';
+        % Q = QFull;  %D = DFull;
+        % Dcomp = [];
     end
 
-    % Z = Q*diag(constants.ev)*Q';
-    Z = Q*diag([constants.ev;Dcomp])*Q';
-    for i = 1:length(constants.A)
+    b = zeros(l,1);
+    for i = 1:l
         b(i,1) = trace((Z-constants.A0)'*constants.A{i});
     end
     xprev = x;
     x = Binv*b;
     % p1 = x-xprev;
-     % p= - Binv*X.J'*X.R;
+    % p= - Binv*X.J'*X.R;
 
     % [x,xRGD]
 
@@ -52,11 +56,12 @@ while stop == false
     % Calculate residual, Jacobian of R
     [X.F,X.R,X.J] = obj_fun(x, constants); FuncCount = FuncCount +1;
 
-    fprintf(repmat('\b',1,OutputLineLength))
-OutputLineLength = fprintf('k = %d; f(x) = %d; |gradf(x)| = %d\n', NIter,X.F,norm(X.J'*X.R));
+    if Verbose
+        fprintf(repmat('\b',1,OutputLineLength))
+        OutputLineLength = fprintf('k = %d; f(x) = %d; |gradf(x)| = %d\n', NIter,X.F,norm(X.J'*X.R));
+    end
 
-
-     [stop, CurrentLoop.ConvergenceFlag] = isminimum(X.F,x, x-xprev,Inf, NIter, constants);
+    [stop, CurrentLoop.ConvergenceFlag] = isminimum(X.F,x, x-xprev,Inf, NIter, constants);
     % pprev=p;
     % Save iterates for plotting
     CurrentLoop.Iterates = [CurrentLoop.Iterates, x];
