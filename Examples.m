6cc36f8116152dc52cbbe27ae45f3e5f4e703514
AlbanBloorRiley <67656764+AlbanBloorRiley@users.noreply.github.com>
2025-04-10 15:19:44 +0100

    Tidied up and commented

--- Examples.m
+++ Examples.m
@@ -1,60 +1,45 @@
 %% Example 1
 clear problem A Binv 
-N =5000;
-l = 40;
+N =5000;  %Dimension of matrices 
+l = 40;   %Number of free parameters
 problem.A0 =sparse(zeros(N));
-
+%Form basis matrices
 A{1} = speye(N);
-Aindexed.i = [1:N]';  Aindexed.j = [1:N]';  Aindexed.v = ones(N,1);
-Aindexed.m = N;Aindexed.n = N;Aindexed.NumV = N;
 for k = 2:l
     A{k} = (spdiags(1,[-k+1,k-1],N,N));
-    nz = nnz(A{k});
-    [Aindexed.i(end+1:end+nz),Aindexed.j(end+1:end+nz),Aindexed.v(end+1:end+nz)] =find(A{k});
-    Aindexed.NumV(k) = nz;
 end
-problem.A = A;
-
-step = 10;
-
-problem.x0 = -ones(l,1);
-
-
+%Set up problem parameters
+problem.A = A;   problem.x0 = -ones(l,1);
 problem.ev = (-1.1:0.002:-1.061)'*1e2;
+problem.StepTolerance= 1e-3; problem.MaxIter = 1000; 
+problem.obj_fun = @IEPMin;
 
-%
-problem.StepTolerance= 1e-3; problem.MaxIter = 1000; repeats = 1;
-%
-problem.obj_fun = @IEPsmallest;
+%Run timings for all three methods
 tic
-for i = 1:repeats
-    RGDLPMinIterations = RGD_LP(problem);
-end
-RGDLPMinTime = toc/repeats
-%
+RGDLPMinIterations = RGD_LP(problem);
+RGDLPMinTime = toc
+
 problem.obj_fun = @IEP;
 tic
-for i = 1:repeats
-    RGDLPIterations = RGD_LP(problem);
-end
-RGDLPTime = toc/repeats
-%
+RGDLPIterations = RGD_LP(problem);
+RGDLPTime = toc
+
 tic
-for i = 1:repeats
-    [LPIterations] = LP(problem);
-end
-LPTime = toc/repeats
+LPIterations = LP(problem);
+LPTime = toc
 
 
 %% Example 2 - Mn12 
 clear all
+%Set up units
 rcm = 29979.2458;    % reciprocal cm to MHz
 meV = rcm*8.065;
+%Define solution
 B20 = -0.0570*meV/3; %(D = 3*B02)
 B40 = (-2.78*10^-6)*meV;
 B44 = (-3.2*10^-6)*meV;
 B22 = (6.8*10^-4)*meV;
-
+%Simulate eigenvalues
 Sys.S = 10;
 Sys.B2 = [B22 0 B20 0 0];        % B(k=2,q) with q = +2,+1,0,-1,-2
 Sys.B4 = [B44 0 0 0 B40 0 0 0 0];  % B(k=4,q) with q = +4,+3,+2,+1,0,-1,-2,-3,-4
@@ -62,34 +47,34 @@ H = ham(Sys,[0,0,0]);  [~,E]=eig(H);
 EE = diag(E);  Exp.ev=EE-EE(1);
 
 %The Stevens Operators
-
 A0 = sparse(21,21);
 A{1} = stev(10,[2,0]);
 A{2} = stev(10,[4,0]);
 A{3} = stev(10,[4,4]);
 A{4} = stev(10,[2,2]);
+%Ground State operator
 A{5} = eye(21);
+%Set up problem parameters
 problem.A = A; problem.ev = EE; problem.A0 = A0;
 problem.x0 = round([B20;B40;B44;B22;-1e6],1,'significant');
 problem.x0 = [-1000,1,1,1,0]';
-
-%%
 problem.StepTolerance= 1e-8; problem.MaxIter = 500; repeats = 100;
-%
-problem.obj_fun = @IEPsmallest;
+
+%Run timings for all three methods
+problem.obj_fun = @IEPMin;
 tic
 for i = 1:repeats
     RGDLPMinIterations = RGD_LP(problem);
 end
 RGDLPMinTime = toc/repeats
-%
+
 problem.obj_fun = @IEP;
 tic
 for i = 1:repeats
     RGDLPIterations = RGD_LP(problem);
 end
 RGDLPTime = toc/repeats
-%
+
 tic
 for i = 1:repeats
     LPIterations = LP(problem);
@@ -99,19 +84,17 @@ LPTime = toc/repeats
 
 %% Example 4 - Cr6 
 clear all
+%Set up units
 rcm = 29979.2458;    % reciprocal cm to MHz
 meV = rcm*8.065;
 
+%Simulate eigenvalues
 N_electrons = 6;
 B20 = (-0.041/3).*meV; % convert from meV to MHz
 B22 = 0.007.*meV; % convert from meV to MHz
-% BValues = [-3304.4;1692.5;353000];
-% We have 6 S=3/2 spins that are coupled together
 S=3/2;
 Sys.S = S;
 for i = 2:N_electrons;Sys.S = [Sys.S,S]; end
-% n = prod(2*Sys.S+1);
-% each of the spins has a non zero B22 and B20 
 B2 = [B22 0 B20 0 0]; % B(k=2,q) with q = +2,+1,0,-1,-2
 B2s = [1 0 1 0 0];
 Sys1.B2 = B2s;
@@ -120,15 +103,17 @@ Jval = 1.46*meV;
 J =[]; %ee = [];
 for i = 2:N_electrons
     J = [1,zeros(1,i-2),J];
-    % ee = [eye(3);zeros(3*(i-2),3);ee];
 end
-% Sys.ee = Jval.*ee;
 Sys1.J = J;
 Sys1.S = Sys.S;
 
 Sys.B2 = Sys1.B2.*B2;
 Sys.J = Jval.*J;
+H=ham(Sys,[0,0,0],'sparse');
+[Vecs,EE] = eig(full(H),'vector');
+Eigs=EE(1:24)-EE(1);
 
+%Form basis matrices
 A{1} = stev(Sys1.S,[2,2,1],'sparse');
 A{2} = stev(Sys1.S,[2,0,1],'sparse');
 for i = 2:N_electrons       
@@ -137,36 +122,23 @@ for i = 2:N_electrons
 end   
 A{3} = ham_ee(Sys1,1:N_electrons,'sparse')*1;
 
-
+%Set up problem parameters
 A0 = sparse(length(A{1}),length(A{1}));
-% A0 = speye(size(A{1}))*5.21e6;
-H=ham(Sys,[0,0,0],'sparse');
-[Vecs,EE] = eig(full(H),'vector');
-Eigs=EE(1:24)-EE(1);
-
 problem.A = A; problem.ev = Eigs; problem.A0 = A0;
 problem.Solver = @mldivide;  problem.obj_fun = @IEP;
 problem.A{4} = speye(size(A{1}));
+
+%Form B and Binverse matrices 
 tic;[Binv]= FormBinv(problem.A);toc
 problem.Binv = Binv;
-% f = spyplots(1,3,A{1},A{2},A{3});
-% f.Units = 'centimeters';
-% f.Position = [-50 10 20 14];
-% print(f, 'Figures/SpyPlots.eps', '-depsc')
+%Form spy plots for Example 3
+f = spyplots(1,3,A{1},A{2},A{3});
+f.Units = 'centimeters';
+f.Position = [-50 10 20 14];
+print(f, 'Figures/SpyPlots.eps', '-depsc')
 %%
-% problem.x0 = [1692.5,-3304,3.53e5,5.2117e6]';
-% problem.x0 = [1000,-3000,353000,5.21e6]';
-% 
-% problem.x0 = [1,-1,1,1]'*1e4;
-% x0 = [1e3,-1e3,3e5,5e6]';
-
-repeats = 1;
+%Set up grid points
 Npoints = 5;
-
-X1 = linspace(0,5e3,Npoints);
-X2 = linspace(0,-5e3,Npoints);
-X3 = linspace(0,5e6,Npoints);
-X4 = linspace(0,5e6,Npoints);
 X1 = logspace(3,7,Npoints);
 X2 = -logspace(3,7,Npoints);
 X3 = logspace(3,7,Npoints);
@@ -175,20 +147,17 @@ X4 = logspace(3,7,Npoints);
 l = 4; 
 NIterates = Npoints^l;
 
-%556 for N =5 3to6
 
-
-
-%%
+%Run timings for all three methods
 RGDMinx = zeros(l,NIterates);
 tic
 for i = 1:NIterates
-    [F,R,J] = IEPsmallest([x1(i);x2(i);x3(i);x4(i)],problem);
+    [F,R,J] = IEPMin([x1(i);x2(i);x3(i);x4(i)],problem);
     p(:,i) = - Binv*J'*R;
     RGDMinx(:,i) = [x1(i);x2(i);x3(i);x4(i)] +p(:,i);
 end
 RGDLPMinTime = toc
-% 
+
 RGDx = zeros(l,NIterates);
 tic
 for i = 1:NIterates
@@ -196,18 +165,16 @@ for i = 1:NIterates
     p = - Binv*J'*R;
     RGDx(:,i) = [x1(i);x2(i);x3(i);x4(i)] +p;
 end
-toc
-%
-x = zeros(l,NIterates);
 RGDLPTime = toc
+
+tic
+x = zeros(l,NIterates);
 for i = 1:NIterates
     [QFull,DFull] = eig(full(FormA([x1(i);x2(i);x3(i);x4(i)],problem.A,problem.A0)),'vector');
     if length(problem.ev)<length(DFull)
         C = (DFull'-problem.ev).^2;
         pairs = matchpairs(C,100*max(max(C)));
-        % D = DFull(pairs(:,2));
         Dcomp = DFull;
-        % Dmatch = DFull(pairs(:,2));
         Dcomp(pairs(:,2))=[];
         Qmatch = QFull(:,pairs(:,2));
         Qcomp = QFull;
@@ -216,8 +183,6 @@ for i = 1:NIterates
          Z = [Qmatch,Qcomp]*diag([problem.ev;Dcomp])*[Qmatch,Qcomp]';
     else
         Z = QFull*diag(problem.ev)*QFull';
-        % Q = QFull;  %D = DFull;
-        % Dcomp = [];
     end
     b = zeros(l,1);
     for j = 1:l
@@ -234,98 +199,65 @@ clear all
 rcm = 29979.2458;    % reciprocal cm to MHz
 meV = rcm*8.065;
 
+%Define Eigenvalues
 problem.ev = [ 0, 0.1414, 0.59070, 0.59070, 1.0841, 1.0841 , 1.0841, 1.4134, 1.4134, 2.316, 2.316, 2.316, 2.5218, 2.5218, 2.5218, 2.5218]'.*meV;  
 problem.ev =  [ 0, 0.1414, 0.59070, 0.59070, 1.0841, 1.0841 , 1.0841, 1.4134, 1.4134, 2.316, 2.316, 2.316, 2.316, 2.316,  2.5218, 2.5218]'.*meV;
 Sys1.S = [2 2 5/2 5/2 5/2 5/2]; % MnIII has S = 2, MnII has S = 5/2
 
-clear A
+%Form basis matrices
  Sys1.J = [10*meV,0 0 0 0 0 0 0 0 0 0 0 0 0 0];
  problem.A0 = ham_ee(Sys1,1:length(Sys1.S),'sparse'); problem.A0 =sparse(32400,32400);
 A{1} = stev(Sys1.S,[2,0,1],'sparse')+ stev(Sys1.S,[2,0,2],'sparse');
-% A{end+1} = stev(Sys1.S,[2,2,1],'sparse')+ stev(Sys1.S,[2,2,1],'sparse');
 Sys1.J = [0,1 0 1 0 0 1 0 1 0 0 0 0 0 0];
 A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse');
 Sys1.J = [0,0 1 0 1 1 0 1 0 0 0 0 0 0 0];
 A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse');
-% A{end} = A{end}-ham_ee(Sys1,1:length(Sys1.S),'sparse');
-
-
 Sys1.J = [0 0 0 0 0 0 0 0 0 1 0 0 0 0 1];
 A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse');
-
 Sys1.J = [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
 A{end+1} = ham_ee(Sys1,1:length(Sys1.S),'sparse'); 
 
-
-problem.Solver = @mldivide;  problem.obj_fun = @IEPsmallest;
-% f = spyplots(2,2,A{1},A{2},A{3},A{4});
-
-%%
-problem.x0 = [-21035 -1.9826e+05 1.9343e+05  48357 ]';
-problem.x0 = [-1e4 -1e+05 1e+05  4e5 ]'*1e-1;
-problem.x0 = [1,1,1,1,1]'*1e2;
+%Set up problem parameters
+problem.Solver = @mldivide;  problem.obj_fun = @IEPMin;
 % problem.x0 = [-21035 -1.9826e+05 1.9343e+05  48357 10*meV]';
-%  problem.x0 = [-21035 -1.9826e+05  48357 ]';
-% problem.x0 = [-3e4 -1e5 1e5   5e4 ]'*1e0;
-% problem.x0 = [-20000 -200000   200000 50000 2000000]';
-% problem.x0(end+1) =  -eigs(FormA(problem.x0,A,problem.A0),1,"smallestreal");
+problem.x0 = [1,1,1,1,1]'*1e2;
 problem.x0(end+1) = 2e7;
 problem.A = A; problem.A{end+1} = speye(32400,32400);
 problem.Binv = FormBinv(problem.A);
-%%
 problem.Verbose = true;
 problem.StepTolerance= 1e-1; problem.MaxIter = 600; 
 problem.StepDifferenceTolerance = 1e-5;problem.StepRelativeTolerance = 1e-7;
-%
+
+%Run timings for RGDLPMin
 tic
     RGDLPMinIterations = RGD_LP(problem);
 Mn6Time = toc
 
-
-%%
-
-
-% colours = colororder;
+%plot convergence of method
 f = figure(1);
 clf
-% semilogy(sum((RGDLPMinIterations.Iterates(:,2:end)-RGDLPMinIterations.Iterates(:,1:end-1)).^2), LineWidth=1.5)
+% semilogy(sum((RGDLPMinIterations.Iterates(:,2:end)-RGDLPMinIterations.Iterates(:,1:end-1)).^2), LineWidth=2)
 [Binv,B] = FormBinv(problem.A);
-hold on
+
 p =(RGDLPMinIterations.Iterates(:,2:end)-RGDLPMinIterations.Iterates(:,1:end-1));
 % semilogy(sqrt(sum(p.^2)./(sum(RGDLPMinIterations.Iterates(:,end-1).^2))));ylabel("||p^k||/||x^k||")
 % semilogy(sqrt(sum(p.^2)));ylabel("||p^k||")
-
 % semilogy(sqrt(sum(B*p.*p,1)));ylabel("||p^k||_g")
 semilogy(sqrt(sum(B*p.*p,1))./sqrt(sum(RGDLPMinIterations.Iterates(:,end-1)'*B*RGDLPMinIterations.Iterates(:,end-1),1)),LineWidth=2);       ylabel("||p^k||_g/||x^k||_g")
-
-%
 yscale('log')
-hold off
-% ylim([0,1e2])
-xlim([0,600])
 xlabel("Iteration")
 grid("on")
 
 
 f.Units = 'centimeters';
 f.Position = [-50 10 20 14];
-linestyleorder('mixedstyles')
-%
-print(f, ["Figures/Mn6Convergence"+num2str(problem.x0')+".png"], '-dpng')
+% linestyleorder('mixedstyles')
 
+% print(f, ["Figures/Mn6Convergence"+num2str(problem.x0')+".png"], '-dpng')
 print(f, "Figures/Mn6Convergence100.eps", '-depsc')
-% hold on
-% semilogy([nan(1,LPIterations.NIter-1),sum((NewtonIterations.Iterates(:,2:end)-NewtonIterations.Iterates(:,1:end-1)).^2)], 'color',colours(1,:),LineWidth=1.5)
-% 
-%%
-[Q,D] = eigs(FormA(LPIterations.FinalPoint, problem.A,problem.A0),length(problem.ev),'smallestreal');
- [Q0,D0] = eigs(FormA(problem.x0, problem.A,problem.A0),length(problem.ev),'smallestreal');
-D0 = diag(D0); D0 =  (D0-D0(1))./meV;
-D = diag(D); D = (D-D(1))./meV;
-MintOpt.Eigs = D;
-MintOpt.Vecs = Q;
 
 %%
+%Can check validity of INS simulation using Mint (https://mlbakerlab.co.uk/mint/)
 % Iterates = problem.x0;
 Iterates = RGDLPMinIterations.FinalPoint;
 Sys.S = [2 2 5/2 5/2 5/2 5/2]; % MnIII has S = 2, MnII has S = 5/2
@@ -342,8 +274,6 @@ J_14 = -0.00.*meV;         J_23 = J_14; %Assumed zero. Only interacts via a piva
 J_13 = -0.00.*meV;         J_24 = J_13; 
 Sys.J = [J_AB J_A1 J_A2 J_A3 J_A4 J_B1 J_B2 J_B3 J_B4 J_12 J_13 J_14 J_23 J_24 J_34].*(-2); %-2JS.S formalism
 DIII = Iterates(1)./3; % MnIII anisotropy. Free Value 
-% DIII = -0
-% .02*meV;
 EIII = 0*DIII;   % MnIII rhombicity. For INS, assume 0.
 B20III = 3*DIII; B22III = EIII; %Converting to Stevens Operator formalism
 DII = -0.00*meV; % MnII anisotropy. For INS, assume 0. 
@@ -388,19 +318,10 @@ MintSys.Coords = Coords;
     [cross_sect,Eigs,Vecs,I_nm] = mint(MintSys,MintExp,MintOpt);
     plotmint(cross_sect, MintExp, colours)
 
-%%
 
 %% Aux Funcs
 
 
-
-
-% function setMarkerNumber(f,n)
-%     for i = 1:length(f.Children)
-%         f.Children(i).MarkerIndices = 1:(floor(length(f.Children(i).MarkerIndices)/n)):f.Children(i).MarkerIndices(end);
-%     end
-% end
-% 
 function f = spyplots(x,y,varargin)
 l = length(varargin);
 if l~= x*y
@@ -413,11 +334,6 @@ for i = 1:l
 end
 end
 
-function A = scaleAx(x,A)
-for i = 1:length(x)
-    A{i} = A{i}*x(i);
-end
-end
 
 function plotmint(cross_sect, MintExp, colours)
 subplot(2,1,1)
@@ -436,16 +352,3 @@ f.Units = 'centimeters';
 f.Position = [10 10 20 25];
 end
 
-function plotek(Iterations,varargin)
-colours = colororder;
-f = figure(1);
-clf
-
-% for i = 1:length(varargin)
-semilogy(sum((Iterations.Iterates(:,2:end)-Iterations.Iterates(:,1:end-1)).^2), LineWidth=1.5)
-hold on
-if nargin>1
-semilogy([nan(1,varargin{1}.NIter-1),sum((RGDNewtonIterations.Iterates(:,2:end)-varargin{1}.Iterates(:,1:end-1)).^2)], 'color',colours(1,:),LineWidth=1.5)
-end
-hold off
-end
